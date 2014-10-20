8190950207c73a69bf28bb809336bf4eba960a48
Sean Gilbert <segilbert@deloitte.com>
2014-10-20 09:36:09 -0400

    added uninstall chocolatey template

---
+++ _templates/chocolateyauto3/__NAME__.portable/tools/chocolateyUninstall.ps1
@@ -0,0 +1,20 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+#Uninstall for one of these: msi
+#
+$packageName = 'Deloitte.Code.Generation' # arbitrary name for the package, used in messages
+# MSI UninstallString found in Windows Registry 
+# Windows 7 +
+# $file = (Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{UPDATE_WITH_GUID}").UninstallString;
+$file = (Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{UPDATE_WITH_GUID}").UninstallString;
+
+#Uninstall for one of these: exe
+# Custom
+
+#Uninstall for VSIX
+# Custom
+
+#Execute Uinstall Chocolatey Command
+Uninstall-ChocolateyPackage $package 'MSI' -file $file
+
+
